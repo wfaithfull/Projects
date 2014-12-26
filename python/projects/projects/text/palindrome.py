fc74c7548012480a2f658a6f84925a1a7e4b92b5
Will Faithfull <will@sourcepulp.com>
2014-12-26 18:12:46 +0000

    whitespace

--- python/projects/projects/text/palindrome.py
+++ python/projects/projects/text/palindrome.py
@@ -3,7 +3,6 @@ def ispalindrome(s):
     from reverse import reverse
     return s == reverse(s)
 
-
 if __name__ == '__main__':
     import sys
     print 'Is a palindrome' if ispalindrome(sys.argv[1]) else 'Not a palindrome'
