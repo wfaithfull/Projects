c92c6dc6007e4efa9a7f9de0536539daa47fea7f
Will Faithfull <will@sourcepulp.com>
2014-12-26 18:54:04 +0000

    removed unused function

--- python/projects/projects/text/vowelcount.py
+++ python/projects/projects/text/vowelcount.py
@@ -1,22 +1,17 @@
 def vowelcount(s):
     dict = 'aeiouy'
     count = 0
-    for v in dict:
-        count += s.count(v)
-    return count
-
-def vowelcountplus(s):
-    dict = 'aeiouy'
     # Makes dictionary for histogram style output
-    counts = {}
+    hist = {}
     for v in dict:
-        counts[v] = s.count(v)
-    return counts
+        hist[v] = s.count(v);
+    return (sum(hist.values()), hist)
 
 def main(args):
     word = args[1]
-    print 'Total: %d vowels' % vowelcount(word)
-    for vowel, count in vowelcountplus(word).iteritems():
+    count, hist = vowelcount(word)
+    print 'Total: %d vowels' % count
+    for vowel, count in hist.iteritems():
         print '%s %s] %d' % (vowel, '=' * count, count)
 
 if __name__ == '__main__':
