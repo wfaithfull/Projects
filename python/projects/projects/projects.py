5ef101c1ba2f8b930897536b476e0b9b5b0c7f63
Will Faithfull <will@sourcepulp.com>
2014-12-26 16:02:00 +0000

    established project structure. Added numbers module.

---
+++ python/projects/projects/projects.py
@@ -0,0 +1,8 @@
+from number import pi2n, fib
+
+print '10th digit of Pi:'
+print pi2n.bbp(10)
+
+print '10th fibonacci number:'
+print 'Classical: %d' % fib.fib(10)
+print 'Recursive: %d' % fib.fibr(10)
