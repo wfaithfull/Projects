02dca85c7ab85483cbbff79a3dd49fde498eb721
Will Faithfull <will@sourcepulp.com>
2014-12-27 19:07:55 +0000

    modified fib to use tuple, eliminating need for temp variable

--- python/projects/projects/number/fib.py
+++ python/projects/projects/number/fib.py
@@ -2,13 +2,9 @@
 
 # classical
 def fib(n):
-    a = 0
-    b = 1
+    a, b = 0, 1
     for i in range(0,n):
-        temp = a
-        a = b
-        b = b + temp
-
+        a, b = b, a + b
     return a;
 
 # recursive
